fc880f3817b6889f15390958dc8b057608ed1912
Luke Bordonaro <lukebo@microsoft.com>
2021-01-26 11:02:41 -0800

    Create PluginsLoader and associated unit tests. (#21)
    * Create PluginsLoader and associated unit tests.
    
    Refactor assembly processing logic to indicate if all
    assemblies found were processed successfully.
    
    Add a DiscoveryStarted method to the IExtensionTypeObserver
    interface, which gets called at the start of a round of
    processing assemblies.
    
    * Use deployment items instead of looking at build output directly in tests
    
    * Add assert messages and use TestContext.WriteLine
    
    * Added some concurrent tests and address PR comments
    
    * Add UnitTest attribute
    
    * Fix csproj whitespace

---
+++ src/Microsoft.Performance.SDK.Runtime.NetCoreApp.Tests/Plugins/MockCustomDataSources/ValidFolderSchema/InstalledPlugins/1.0.0/ValidA/1.0.0/ValidSchemaA1.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Performance.SDK.Processing;
+using System.Collections.Generic;
+
+namespace Microsoft.Performance.SDK.Runtime.NetCoreApp.Tests.Plugins.MockCustomDataSources
+{
+    [CustomDataSource(
+    "{AAC2FD6A-6AA3-4BF8-BD74-D4D2AF7EA6D8}", "Mock CDS - Valid A v1.0.0", "A mock valid data source A1.")]
+    [FileDataSource(".sdk")]
+    public class ValidSchemaA1
+    : CustomDataSourceBase
+    {
+        protected override ICustomDataProcessor CreateProcessorCore(IEnumerable<IDataSource> dataSources, IProcessorEnvironment processorEnvironment, ProcessorOptions options)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool IsFileSupportedCore(string path)
+        {
+            return true;
+        }
+
+        protected override void SetApplicationEnvironmentCore(IApplicationEnvironment applicationEnvironment)
+        {
+            return;
+        }
+    }
+}
