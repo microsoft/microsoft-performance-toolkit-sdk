e64f18fa55bf6ce2abd84d5bb954c5b9b8ae8c65
Luke Bordonaro <lukebo@microsoft.com>
2021-01-29 14:42:29 -0800

    Add SqlPlugin sample (#31)
    * Add SqlPlugin sample
    
    * Fix repeated GUIDs and whitespace in tables
    
    * Use an XmlReader to parse XML file
    
    * Add video links to README
    
    * Remove IKeyedDataType methods from simple SqlEvent
    
    * Use more efficient IsFileSupportedCore implementation
    
    * Address PR comments

---
+++ samples/SqlPlugin/SqlPluginWithProcessingPipeline/SqlCustomDataSourceWithSourceParser.cs
@@ -0,0 +1,67 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Performance.SDK.Processing;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace SqlPluginWithProcessingPipeline
+{
+    [CustomDataSource("D075CBD0-EAB5-41CD-81FF-66FF590D5089",
+                      "SQL Trace Data Source With Data Cookers",
+                      "Processes SQL trace files exported as XML.")]
+    [FileDataSource(".xml", "XML files exported from TRC files")]
+    public class SqlCustomDataSourceWithSourceParser
+        : CustomDataSourceBase
+    {
+        private IApplicationEnvironment applicationEnvironment;
+
+        protected override ICustomDataProcessor CreateProcessorCore(IEnumerable<IDataSource> dataSources,
+                                                                    IProcessorEnvironment processorEnvironment,
+                                                                    ProcessorOptions options)
+        {
+            //
+            // Since this is a demo, we are going to assume only one file is opened
+            // at a time and just get the .First() data source passed in. For a real
+            // plugin, every data source should be used.
+            //
+
+            var filePath = dataSources.First().GetUri().LocalPath;
+            var parser = new SqlSourceParser(filePath);
+            return new SqlCustomDataProcessorWithSourceParser(parser,
+                                                              options,
+                                                              this.applicationEnvironment,
+                                                              processorEnvironment,
+                                                              this.AllTables,
+                                                              this.MetadataTables);
+        }
+
+        protected override bool IsFileSupportedCore(string path)
+        {
+            // Peek inside the XML and make sure our XML namespace is declared and used
+
+            using (var reader = new StreamReader(path))
+            {
+                // Skip first line since namespace should be on second
+                reader.ReadLine();
+
+                var line = reader.ReadLine();
+
+                if (line != null)
+                {
+                    return line.Contains(SqlPluginConstants.SqlXmlNamespace);
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        protected override void SetApplicationEnvironmentCore(IApplicationEnvironment applicationEnvironment)
+        {
+            this.applicationEnvironment = applicationEnvironment;
+        }
+    }
+}
---
+++ samples/SqlPlugin/SqlPluginWithProcessingPipeline/SqlSourceParser.cs
@@ -0,0 +1,189 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Performance.SDK;
+using Microsoft.Performance.SDK.Extensibility.SourceParsing;
+using Microsoft.Performance.SDK.Processing;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Xml;
+
+namespace SqlPluginWithProcessingPipeline
+{
+    public class SqlSourceParser
+            : ISourceParser<SqlEvent, SqlSourceParser, string>
+    {
+        // XML file to be parsed. For this demo, we assume we only have one data source.
+        // For a full implementation, this should be a collection of all file paths given
+        // to the Custom Data Source
+        private readonly string filePath;
+
+        // Information about this data source the SDK requires for building tables
+        private DataSourceInfo dataSourceInfo;
+
+        public SqlSourceParser(string filePath)
+        {
+            this.filePath = filePath;
+        }
+
+        public DataSourceInfo DataSourceInfo => this.dataSourceInfo;
+
+        public DateTime StartWallClockUtc { get; private set; }
+
+        public string Id => SqlPluginConstants.ParserId;
+
+        public Type DataElementType => typeof(SqlEvent);
+
+        public Type DataContextType => typeof(SqlSourceParser);
+
+        public Type DataKeyType => typeof(string);
+
+        // For this example, we do not need to parse more than once
+        public int MaxSourceParseCount => 1;
+
+        public void PrepareForProcessing(bool allEventsConsumed, IReadOnlyCollection<string> requestedDataKeys)
+        {
+            // NOOP here; process everything :)
+        }
+
+        public void ProcessSource(ISourceDataProcessor<SqlEvent, SqlSourceParser, string> dataProcessor,
+                                                       ILogger logger,
+                                                       IProgress<int> progress,
+                                                       CancellationToken cancellationToken)
+        {
+            using (FileStream stream = File.OpenRead(this.filePath))
+            using (XmlReader reader = XmlReader.Create(stream))
+            {
+                bool inEvents = false;
+                string eventClass = null;
+                DateTime? traceStartTime = null;
+                DateTime? traceEndTime = null;
+
+                string textData = string.Empty, applicationName = string.Empty, ntUserName = string.Empty, loginName = string.Empty;
+                int? cpu = null, reads = null, writes = null, duration = null, clientProcessId = null, spid = null;
+                DateTime? startTime = null, endTime = null;
+
+                while (reader.Read())
+                {
+                    switch (reader.NodeType)
+                    {
+                        case XmlNodeType.Element:
+
+                            if (reader.Name == "Events")
+                            {
+                                inEvents = true;
+                            }
+                            else if (inEvents && reader.Name == "Event")
+                            {
+                                eventClass = reader.GetAttribute("name");
+                            }
+                            else if (eventClass != null && reader.Name == "Column")
+                            {
+                                switch (reader.GetAttribute("name"))
+                                {
+                                    case "TextData":
+                                        textData = reader.ReadElementContentAsString();
+                                        break;
+                                    case "ApplicationName":
+                                        applicationName = reader.ReadElementContentAsString();
+                                        break;
+                                    case "NTUserName":
+                                        ntUserName = reader.ReadElementContentAsString();
+                                        break;
+                                    case "LoginName":
+                                        loginName = reader.ReadElementContentAsString();
+                                        break;
+                                    case "CPU":
+                                        cpu = reader.ReadElementContentAsInt();
+                                        break;
+                                    case "Reads":
+                                        reads = reader.ReadElementContentAsInt();
+                                        break;
+                                    case "Writes":
+                                        writes = reader.ReadElementContentAsInt();
+                                        break;
+                                    case "Duration":
+                                        duration = reader.ReadElementContentAsInt();
+                                        break;
+                                    case "ClientProcessID":
+                                        clientProcessId = reader.ReadElementContentAsInt();
+                                        break;
+                                    case "SPID":
+                                        spid = reader.ReadElementContentAsInt();
+                                        break;
+                                    case "StartTime":
+                                        startTime = reader.ReadElementContentAsDateTime().ToUniversalTime();
+
+                                        if (!traceStartTime.HasValue)
+                                        {
+                                            traceStartTime = startTime;
+                                            this.StartWallClockUtc = startTime.Value;
+                                        }
+                                        break;
+                                    case "EndTime":
+                                        endTime = reader.ReadElementContentAsDateTime().ToUniversalTime();
+                                        traceEndTime = endTime;
+                                        break;
+
+                                }
+                            }
+
+                            break;
+
+                        case XmlNodeType.EndElement:
+
+                            if (reader.Name == "Events")
+                            {
+                                inEvents = false;
+                            }
+                            else if (inEvents && eventClass != null && reader.Name == "Event")
+                            {                                
+                                var sqlEvent = new SqlEvent(eventClass,
+                                            textData,
+                                            applicationName,
+                                            ntUserName,
+                                            loginName,
+                                            cpu,
+                                            reads,
+                                            writes,
+                                            duration,
+                                            clientProcessId,
+                                            spid,
+                                            startTime.Value,
+                                            endTime);
+                                eventClass = null;
+                                textData = string.Empty;
+                                applicationName = string.Empty;
+                                ntUserName = string.Empty; 
+                                loginName = string.Empty;
+                                cpu = null;
+                                reads = null;
+                                writes = null;
+                                duration = null;
+                                clientProcessId = null;
+                                spid = null;
+                                startTime = null;
+                                endTime = null;
+
+                                dataProcessor.ProcessDataElement(sqlEvent, this, cancellationToken);
+                            }
+
+                            break;
+                    }
+
+                    progress.Report((int)(100.0 * stream.Position / stream.Length));
+                }
+
+                if (traceStartTime.HasValue && traceEndTime.HasValue)
+                {
+                    var traceStartTimestamp = Timestamp.FromNanoseconds(traceStartTime.Value.Ticks * 100);
+                    var traceEndTimestamp = Timestamp.FromNanoseconds(traceEndTime.Value.Ticks * 100);
+
+                    this.dataSourceInfo = new DataSourceInfo(0, (traceEndTimestamp - traceStartTimestamp).ToNanoseconds, traceStartTime.Value);
+                }
+            }         
+        }
+    }
+}
