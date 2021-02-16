e5600d5456193ed48fd453c07d02caf1d4062ff9
Zack Newman <zachnew@microsoft.com>
2021-02-16 08:04:30 -0800

    Update documentation. Remove hash set.

--- src/Microsoft.Performance.SDK.Runtime/DataSourceResolver.cs
+++ src/Microsoft.Performance.SDK.Runtime/DataSourceResolver.cs
@@ -8,36 +8,40 @@ using Microsoft.Performance.SDK.Processing;
 namespace Microsoft.Performance.SDK.Runtime
 {
     /// <summary>
-    ///     Resolves the Custom Data Sources that can process given Data Sources.
+    ///     Resolves the <see cref="CustomDataSourceReference" />s that can
+    ///     process a given set of <see cref="IDataSource"/>s.
     /// </summary>
     public static class DataSourceResolver
     {
         /// <summary>
         ///     Given a collection of <see cref="IDataSource"/>s and
         ///     <see cref="CustomDataSourceReference"/>s, creates a mapping
-        ///     that assigns each Data Source to one or more Custom Data Sources
-        ///     on the basis of what can be processed by each Custom Data Source.
+        ///     that assigns each <see cref="IDataSource"/> to one or more
+        ///     <see cref="CustomDataSourceReference"/>s on the basis of what 
+        ///     can be processed by each Custom Data Source.
         ///     <para />
-        ///     Each Custom Data Source in <paramref name="customDataSources"/> will be a
-        ///     key in the returned mapping. The value for any given key represents
-        ///     the Data Sources that can be processed the given Custom Data Source. If
-        ///     no Data Sources can be processed by a given Custom Data Source, then the
+        ///     Each <see cref="CustomDataSourceReference"/> in <paramref name="customDataSources"/>
+        ///     will be a key in the returned mapping. The value for any given key represents
+        ///     the <see cref="IDataSource"/>s that can be processed by the given 
+        ///     <see cref="CustomDataSourceReference"/>/. If no <see cref="IDataSource"/>s
+        ///     can be processed by a given <see cref="CustomDataSourceReference"/>, then the
         ///     value will be an empty collection.
         /// </summary>
         /// <param name="dataSources">
-        ///     The Data Sources to be mappedt o the Custom Data Sources that are able
-        ///     to process them. Any duplicate values in this parameter will be
-        ///     ignored. Any null elements in this parameter will be ignored.
+        ///     The <see cref="IDataSource"/>s to be mapped to the <see cref="CustomDataSourceReference"/>s
+        ///     that are able to process them. Any duplicate values in this parameter will be
+        ///     ignored. Any <c>null</c> elements in this parameter will be ignored.
         /// </param>
         /// <param name="customDataSources">
-        ///     The collection of Custom Data Source references that are potentially
+        ///     The collection of <see cref="CustomDataSourceReference"/>s that are potentially
         ///     eligible to process the elements of <paramref name="dataSources"/>.
-        ///     Any duplicate values in this parameter will be ignored. Any null elements
+        ///     Any duplicate values in this parameter will be ignored. Any <c>null</c> elements
         ///     in this parameter will be ignored.
         /// </param>
         /// <returns>
-        ///     A mapping of Custom Data Sources to collections of zero (0) or more
-        ///     Data Sources that can be processed by said Custom Data Sources.
+        ///     A mapping of <see cref="CustomDataSourceReference"/>s to collections of zero (0) 
+        ///     or more <see cref="IDataSource"/> that can be processed by said 
+        ///     <see cref="CustomDataSourceReference"/>s.
         /// </returns>
         /// <exception cref="System.ArgumentNullException">
         ///     <paramref name="dataSources"/> is <c>null</c>.
@@ -51,8 +55,8 @@ namespace Microsoft.Performance.SDK.Runtime
             Guard.NotNull(dataSources, nameof(dataSources));
             Guard.NotNull(customDataSources, nameof(customDataSources));
 
-            var dedupedDs = new HashSet<IDataSource>(dataSources.Where(x => x is object));
-            var dedupedCds = new HashSet<CustomDataSourceReference>(customDataSources.Where(x => x is object));
+            var dedupedDs = dataSources.Where(x => x is object).Distinct().ToList();
+            var dedupedCds = customDataSources.Where(x => x is object).Distinct().ToList();
 
             bool isSupported(CustomDataSourceReference cdsr, IDataSource dataSource)
             {
