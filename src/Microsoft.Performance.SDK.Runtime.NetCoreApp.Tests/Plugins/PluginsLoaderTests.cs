f2ca7860ea1cfd5892cd3a0924188cbc5372f7dd
Tristan Gibeau <41077386+trgibeau@users.noreply.github.com>
2021-04-07 09:32:26 -0700

    Fix paths in tests when running on non Windows (#69)
    * Fix Paths in Tests
    
    * Fix DeploymentItem Attribute

--- src/Microsoft.Performance.SDK.Runtime.NetCoreApp.Tests/Plugins/PluginsLoaderTests.cs
+++ src/Microsoft.Performance.SDK.Runtime.NetCoreApp.Tests/Plugins/PluginsLoaderTests.cs
@@ -47,10 +47,12 @@ namespace Microsoft.Performance.SDK.Runtime.NetCoreApp.Plugins.Tests
         [ClassInitialize]
         public static void Initialize(TestContext context)
         {
-            var rootDir = Path.Combine(context.DeploymentDirectory, "TestPluginsSourceCode");
+            var fixedDeploymentDirectory = Path.Combine(context.DeploymentDirectory.Split('\\', StringSplitOptions.RemoveEmptyEntries));
+
+            var rootDir = Path.Combine(fixedDeploymentDirectory, "TestPluginsSourceCode");
 
             // Folder to store generated DLLs
-            compilePath = Path.Combine(context.DeploymentDirectory, "TestPluginsBinaries");
+            compilePath = Path.Combine(fixedDeploymentDirectory, "TestPluginsBinaries");
             if (Directory.Exists(compilePath))
             {
                 Directory.Delete(compilePath, true);
