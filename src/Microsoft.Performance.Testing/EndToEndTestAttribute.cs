b132c18be0bfb757f357868633cbbbd7d72f4a67
Zack Newman <zachnew@microsoft.com>
2021-07-02 09:40:45 -0700

    Add E2E test attribute.

---
+++ src/Microsoft.Performance.Testing/EndToEndTestAttribute.cs
@@ -0,0 +1,37 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Microsoft.Performance.Testing
+{
+    /// <summary>
+    ///     Marks a test as being an automated end to end test. This gives
+    ///     the test method the "EndToEndTest" trait.
+    /// </summary>
+    public class EndToEndTestAttribute
+        : TestCategoryBaseAttribute
+    {
+        private static readonly ReadOnlyCollection<string> testCategories =
+            new ReadOnlyCollection<string>(
+                new[]
+                {
+                    "EndToEndTest",
+                });
+
+        public EndToEndTestAttribute()
+        {
+        }
+
+        /// <inheritdoc />
+        public override IList<string> TestCategories
+        {
+            get
+            {
+                return testCategories;
+            }
+        }
+    }
+}
