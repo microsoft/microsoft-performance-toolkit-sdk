bd3625165d8886b39011bb16a9ae8b318629a346
Jayson Maxson <jmaxson@ntdev.microsoft.com>
2021-09-08 10:14:48 -0700

    Add a TableDescriptorFactory.Create method.

---
+++ src/Microsoft.Performance.SDK/Extensibility/ExtensibilityException.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.Performance.SDK.Extensibility
+{
+    /// <summary>
+    ///     Base class for all SDK extensibility exceptions.
+    /// </summary>
+    public class ExtensibilityException : Exception
+    {
+        public ExtensibilityException(string message)
+            : base(message)
+        {
+        }
+
+        public ExtensibilityException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    /// <summary>
+    ///     There is a problem with a table extension.
+    /// </summary>
+    public class ExtendedTableException : ExtensibilityException
+    {
+        public ExtendedTableException(string message)
+            : base(message)
+        {
+        }
+    }
+}
