3be8e1df8cd28c2a7bc8ee7e7e55eaca623ef477
Zack Newman <zachnew@microsoft.com>
2021-09-09 12:00:38 -0700

    Update results API to return a more streamlined object for data queries.

---
+++ src/Microsoft.Performance.Toolkit.Engine/CookedData.cs
@@ -0,0 +1,117 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using Microsoft.Performance.SDK.Extensibility;
+using Microsoft.Performance.SDK.Extensibility.DataCooking;
+
+namespace Microsoft.Performance.Toolkit.Engine
+{
+    /// <summary>
+    ///     Retrieves data from a single cooker instance.
+    /// </summary>
+    public sealed class CookerOutput
+    {
+        private readonly DataCookerPath cookerPath;
+        private readonly ICookedDataRetrieval retrieval;
+
+        internal CookerOutput(
+            DataCookerPath cookerPath,
+            ICookedDataRetrieval retrieval)
+        {
+            this.cookerPath = cookerPath;
+            this.retrieval = retrieval;
+        }
+
+        /// <summary>
+        ///     Retrieves data by name, typecast to the given type.
+        ///     The caller is coupled to the data extension and expected to know the
+        ///     data type.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     Type of the data to retrieve.
+        /// </typeparam>
+        /// <param name="name">
+        ///     The name of the data output.
+        /// </param>
+        /// <returns>
+        ///     The uniquely identified data.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        ///     <paramref name="name"/> references data that does not exist.
+        /// </exception>
+        /// <exception cref="InvalidCastException">
+        ///     The value at <paramref name="name"/> cannot be cast to <typeparamref name="T"/>.
+        /// </exception>
+        public T QueryOutput<T>(string name)
+        {
+            var p = CreateOutputPath(this.cookerPath, name);
+            return this.retrieval.QueryOutput<T>(p);
+        }
+
+        /// <summary>
+        ///     Retrieves data by name.
+        /// </summary>
+        /// <param name="name">
+        ///     Unique name of the data to retrieve.
+        /// </param>
+        /// <returns>
+        ///     The uniquely identified data, as an <see cref="object"/>.
+        /// </returns>
+        /// <exception cref="InvalidOperationException">
+        ///     <paramref name="name"/> references data that does not exist.
+        /// </exception>
+        public object QueryOutput(string name)
+        {
+            var p = CreateOutputPath(this.cookerPath, name);
+            return this.retrieval.QueryOutput(p);
+        }
+
+        /// <summary>
+        ///     Retrieves data by name, typecast to the given type.
+        ///     The caller is coupled to the data extension and expected to know the
+        ///     data type.
+        /// </summary>
+        /// <typeparam name="T">
+        ///     Type of the data to retrieve.
+        /// </typeparam>
+        /// <param name="name">
+        ///     Unique name of the data to retrieve.
+        /// </param>
+        /// <param name="result">
+        ///     The uniquely identified data.
+        /// </param>
+        /// <returns>
+        ///     <c>true</c> if the value was successfully retrieved; <c>false</c> otherwise.
+        /// </returns>
+        public bool TryQueryOutput<T>(string name, out T result)
+        {
+            var p = CreateOutputPath(this.cookerPath, name);
+            return this.retrieval.TryQueryOutput<T>(p, out result);
+        }
+
+        /// <summary>
+        ///     Retrieves data by name.
+        /// </summary>
+        /// <param name="name">
+        ///     Unique name of the data to retrieve.
+        /// </param>
+        /// <param name="result">
+        ///     The uniquely identified data, as an object.
+        /// </param>
+        /// <returns>
+        ///     <c>true</c> if the value was successfully retrieved; <c>false</c> otherwise.
+        /// </returns>
+        public bool TryQueryOutput(string name, out object result)
+        {
+            var p = CreateOutputPath(this.cookerPath, name);
+            return this.retrieval.TryQueryOutput(p, out result);
+        }
+
+        private static DataOutputPath CreateOutputPath(DataCookerPath cookerPath, string name)
+        {
+            return cookerPath.IsSource
+                ? DataOutputPath.ForSource(cookerPath.SourceParserId, cookerPath.DataCookerId, name)
+                : DataOutputPath.ForComposite(cookerPath.DataCookerId, name);
+        }
+    }
+}
--- src/Microsoft.Performance.Toolkit.Engine/EngineExecutionResults.cs
+++ src/Microsoft.Performance.Toolkit.Engine/EngineExecutionResults.cs
@@ -79,23 +79,10 @@ namespace Microsoft.Performance.Toolkit.Engine
         /// <exception cref="CookerNotFoundException">
         ///     <paramref name="cookerPath"/> does not represent a known cooker.
         /// </exception>
-        public ICookedDataRetrieval GetCookedData(DataCookerPath cookerPath)
+        public CookerOutput GetCookedData(DataCookerPath cookerPath)
         {
-            if (this.sourceCookers.Contains(cookerPath))
-            {
-                return this.cookedDataRetrieval;
-            }
-
-            try
-            {
-                var cooker = this.repository.GetCompositeDataCookerReference(cookerPath);
-                var retrieval = this.retrievalFactory.CreateDataRetrievalForCompositeDataCooker(cookerPath);
-                return cooker.GetOrCreateInstance(retrieval);
-            }
-            catch (Exception e)
-            {
-                throw new CookerNotFoundException(cookerPath, e);
-            }
+            var r = this.GetRetrieval(cookerPath);
+            return new CookerOutput(cookerPath, r);
         }
 
         /// <summary>
@@ -112,7 +99,7 @@ namespace Microsoft.Performance.Toolkit.Engine
         ///     <c>true</c> if the cooker can be queried;
         ///     <c>false</c> otherwise.
         /// </returns>
-        public bool TryGetCookedData(DataCookerPath cookerPath, out ICookedDataRetrieval retrieval)
+        public bool TryGetCookedData(DataCookerPath cookerPath, out CookerOutput retrieval)
         {
             try
             {
@@ -162,8 +149,8 @@ namespace Microsoft.Performance.Toolkit.Engine
         {
             try
             {
-                var cooker = this.GetCookedData(dataOutputPath.CookerPath);
-                return cooker.QueryOutput(dataOutputPath);
+                var retrieval = this.GetRetrieval(dataOutputPath.CookerPath);
+                return retrieval.QueryOutput(dataOutputPath);
             }
             catch (Exception e)
             {
@@ -331,6 +318,26 @@ namespace Microsoft.Performance.Toolkit.Engine
                 return false;
             }
         }
+
+        private ICookedDataRetrieval GetRetrieval(DataCookerPath cookerPath)
+        {
+            if (this.sourceCookers.Contains(cookerPath))
+            {
+                return this.cookedDataRetrieval;
+            }
+
+            try
+            {
+                var cooker = this.repository.GetCompositeDataCookerReference(cookerPath);
+                var retrieval = this.retrievalFactory.CreateDataRetrievalForCompositeDataCooker(cookerPath);
+                return cooker.GetOrCreateInstance(retrieval);
+            }
+            catch (Exception e)
+            {
+                throw new CookerNotFoundException(cookerPath, e);
+            }
+        }
+
 
         // Will perform the Func on the Repository if the table is contained, else false.
         private bool ExecuteOnRepositoryIfContained<TResult>(TableDescriptor tableDescriptor, Func<ITableExtensionReference, IDataExtensionRetrieval, TResult> func, out TResult result)
