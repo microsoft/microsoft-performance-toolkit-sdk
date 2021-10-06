48c6d7310b553ee2e9717468bf4ce51dbf9c331e
Zack Newman <zachnew@microsoft.com>
2021-10-06 08:14:27 -0700

    Move TableDiscovery to its own file.

--- src/Microsoft.Performance.SDK/Processing/TableDiscovery.cs
+++ src/Microsoft.Performance.SDK/Processing/TableDiscovery.cs
@@ -6,28 +6,9 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
-using Microsoft.Performance.SDK.Extensibility;
 
 namespace Microsoft.Performance.SDK.Processing
 {
-    /// <summary>
-    ///     Provides an interface for providing tables that are exposed
-    ///     by <see cref="ProcessingSource"/>s.
-    /// </summary>
-    public interface ITableProvider
-    {
-        /// <summary>
-        ///     Returns the collection of tables that should be associated with a Processing Source.
-        /// </summary>
-        /// <param name="tableConfigSerializer">
-        ///     The serializer used to deserialize table configurations.
-        /// </param>
-        /// <returns>
-        ///     A collection of tables.
-        /// </returns>
-        ISet<DiscoveredTable> Discover(ISerializer tableConfigSerializer);
-    }
-
     /// <summary>
     ///     Provides common <see cref="ITableProvider"/> implementations.
     /// </summary>
@@ -195,115 +176,4 @@ namespace Microsoft.Performance.SDK.Processing
         }
     }
 
-    /// <summary>
-    ///     Represents a table discovered by an <see cref="ITableProvider"/>
-    ///     instance.
-    /// </summary>
-    public sealed class DiscoveredTable
-    {
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="DiscoveredTable"/>
-        ///     class, indicating that the table with the given <see cref="TableDescriptor"/>
-        ///     exists, and uses the default build action.
-        /// </summary>
-        /// <param name="descriptor">
-        ///     The descriptor of the table.
-        /// </param>
-        /// <exception cref="System.ArgumentNullException">
-        ///     <paramref name="descriptor"/> is <c>null</c>.
-        /// </exception>
-        public DiscoveredTable(TableDescriptor descriptor)
-            : this(descriptor, null)
-        {
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="DiscoveredTable"/>
-        ///     class, indicating that the table with the given <see cref="TableDescriptor"/>
-        ///     exists, and is built via the specified build action.
-        /// </summary>
-        /// <param name="descriptor">
-        ///     The descriptor of the table.
-        /// </param>
-        /// <param name="buildTable">
-        ///     The build action for the table. This parameter may be <c>null</c> to specify
-        ///     that the default build action should be used.
-        /// </param>
-        /// <exception cref="System.ArgumentNullException">
-        ///     <paramref name="descriptor"/> is <c>null</c>.
-        /// </exception>
-        public DiscoveredTable(TableDescriptor descriptor, Action<ITableBuilder, IDataExtensionRetrieval> buildTable)
-            : this(descriptor, buildTable, false)
-        {
-        }
-
-        /// <summary>
-        ///     Initializes a new instance of the <see cref="DiscoveredTable"/>
-        ///     class, indicating that the table with the given <see cref="TableDescriptor"/>
-        ///     exists, and is built via the specified build action.
-        /// </summary>
-        /// <param name="descriptor">
-        ///     The descriptor of the table.
-        /// </param>
-        /// <param name="buildTable">
-        ///     The build action for the table. This parameter may be <c>null</c> to specify
-        ///     that the default build action should be used.
-        /// </param>
-        /// <param name="isInternal">
-        ///     A value indicating whether this table is internal to the plugin.
-        /// </param>
-        /// <exception cref="System.ArgumentNullException">
-        ///     <paramref name="descriptor"/> is <c>null</c>.
-        /// </exception>
-        public DiscoveredTable(
-            TableDescriptor descriptor, 
-            Action<ITableBuilder, IDataExtensionRetrieval> buildTable,
-            bool isInternal)
-        {
-            Guard.NotNull(descriptor, nameof(descriptor));
-
-            this.Descriptor = descriptor;
-            this.BuildTable = buildTable;
-            this.IsInternal = isInternal;
-        }
-
-        /// <summary>
-        ///     Gets the <see cref="TableDescriptor"/> of the table.
-        /// </summary>
-        public TableDescriptor Descriptor { get; }
-
-        /// <summary>
-        ///     Gets the build action of the table. This property may be <c>null</c>.
-        /// </summary>
-        public Action<ITableBuilder, IDataExtensionRetrieval> BuildTable { get; }
-
-        /// <summary>
-        ///     Gets a value indicating whether this table is internal to the plugin.
-        /// </summary>
-        public bool IsInternal { get; }
-
-        /// <summary>
-        ///     Gets a value indicating whether this table is a metadata table.
-        /// </summary>
-        public bool IsMetadata => this.Descriptor.IsMetadataTable;
-
-        /// <inheritdoc />
-        public override bool Equals(object obj)
-        {
-            if (ReferenceEquals(this, obj))
-            {
-                return true;
-            }
-
-            var other = obj as DiscoveredTable;
-            return other != null &&
-                   this.Descriptor.Equals(other.Descriptor);
-        }
-
-        /// <inheritdoc />
-        public override int GetHashCode()
-        {
-            return this.Descriptor.GetHashCode();
-        }
-    }
 }
