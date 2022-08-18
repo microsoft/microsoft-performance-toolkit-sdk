b6db1cf5f15bfc62f9d3e986c153b02fca84d2b5
Luke Bordonaro <lukebo@microsoft.com>
2022-08-18 11:51:01 -0700

    Add IDataSourceGrouper

---
+++ src/Microsoft.Performance.SDK/Processing/DataSourceGrouping/IDataSourceGrouper.cs
@@ -0,0 +1,31 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using Microsoft.Performance.SDK.Processing;
+using Microsoft.Performance.SDK.Processing.DataSourceGrouping;
+
+namespace Microsoft.Performance.SDK.Processing.DataSourceGrouping
+{
+    /// <summary>
+    ///     Responsible for grouping disparate <see cref="IDataSource"/> instances into <see cref="IDataSourceGroup"/>
+    ///     instances.
+    /// </summary>
+    public interface IDataSourceGrouper
+    {
+        /// <summary>
+        ///     Groups the specified <paramref name="dataSources" /> into groups that each can be processed by a
+        ///     <see cref="ICustomDataProcessor" /> as a single, combined <see cref="IDataSource" />.
+        ///     <para />
+        ///     The returned value must include every valid combination of <see cref="IDataSource" />s in the given
+        ///     <paramref name="dataSources" />.
+        /// </summary>
+        /// <param name="dataSources">
+        ///     The <see cref="IDataSource"/> instances to be grouped.
+        /// </param>
+        /// <returns>
+        ///     All valid groups of <see cref="IDataSource"/>s.
+        /// </returns>
+        IReadOnlyCollection<IDataSourceGroup> Group(IEnumerable<IDataSource> dataSources);
+    }
+}
