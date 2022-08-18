6e70f5ce95bcd2de06f3487b359ec44fa8647c3e
Luke Bordonaro <lukebo@microsoft.com>
2022-08-18 11:50:06 -0700

    Add IDataSourceGroup and DataSourceGroup

---
+++ src/Microsoft.Performance.SDK/Processing/DataSourceGrouping/DataSourceGroup.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Performance.SDK.Processing.DataSourceGrouping
+{
+    /// <inheritdoc/>
+    public sealed class DataSourceGroup
+        : IDataSourceGroup
+    {
+        public DataSourceGroup(IReadOnlyCollection<IDataSource> dataSources, IProcessingMode processingMode)
+        {
+            DataSources = dataSources;
+            ProcessingMode = processingMode;
+        }
+
+        /// <inheritdoc/>
+        public IReadOnlyCollection<IDataSource> DataSources { get; }
+        
+        /// <inheritdoc/>
+        public IProcessingMode ProcessingMode { get; }
+    }
+}
---
+++ src/Microsoft.Performance.SDK/Processing/DataSourceGrouping/IDataSourceGroup.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Performance.SDK.Processing.DataSourceGrouping
+{
+    /// <summary>
+    ///     A <see cref="IDataSource"/> collection that a <see cref="ICustomDataProcessor"/> can process together as
+    ///     a single, combined <see cref="IDataSource"/> in the specified <see cref="ProcessingMode"/>.
+    /// </summary>
+    public interface IDataSourceGroup
+    {
+        /// <summary>
+        ///     Gets the <see cref="IDataSource"/>s in this group.
+        /// </summary>
+        IReadOnlyCollection<IDataSource> DataSources { get; }
+        
+        /// <summary>
+        ///     
+        /// </summary>
+        IProcessingMode ProcessingMode { get; }
+    }
+}
