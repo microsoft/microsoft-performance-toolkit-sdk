e3a2c967dce71727f66a7119559a16c6fa22bffb
Luke Bordonaro <lukebo@microsoft.com>
2022-08-18 11:55:17 -0700

    Add DisjointGroupsDataSourceGrouper

---
+++ src/Microsoft.Performance.SDK/Processing/DataSourceGrouping/DisjointGroupsDataSourceGrouper.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Microsoft.Performance.SDK.Processing.DataSourceGrouping
+{
+    /// <summary>
+    ///     A default <see cref="IDataSourceGrouper"/> that places each <see cref="IDataSource"/> in its own group
+    ///     with the <see cref="DefaultProcessingMode"/>.
+    /// </summary>
+    public class DisjointGroupsDataSourceGrouper
+        : IDataSourceGrouper
+    {
+        /// <summary>
+        ///     Returns a 1-to-1 mapping of <paramref name="dataSources"/> items to <see cref="IDataSourceGroup"/>s. Each
+        ///     <see cref="IDataSource"/> is placed in its own <see cref="IDataSourceGroup"/>, indicating no two instances
+        ///     can be processed together.
+        /// </summary>
+        /// <param name="dataSources">
+        ///     The <see cref="IDataSource"/> instances to be grouped.
+        /// </param>
+        /// <returns>
+        ///     A <see cref="IDataSourceGroup"/> for each item in <paramref name="dataSources"/>.
+        /// </returns>
+        public IReadOnlyCollection<IDataSourceGroup> Group(IEnumerable<IDataSource> dataSources)
+        {
+            return dataSources
+                .Select(dataSource => new DataSourceGroup(new IDataSource[] { dataSource }.ToList(), new DefaultProcessingMode()))
+                .ToList();
+        }
+    }
+}
