33edc798fa1942243d7163f5833d128ef84f73a3
Luke Bordonaro <lukebo@microsoft.com>
2022-09-08 13:05:01 -0700

    Remove SupportsGroupCombining

--- src/Microsoft.Performance.SDK/Processing/IProcessingMode.cs
+++ src/Microsoft.Performance.SDK/Processing/IProcessingMode.cs
@@ -26,18 +26,5 @@ namespace Microsoft.Performance.SDK.Processing
         ///     Gets a human-readable description of this processing mode.
         /// </summary>
         string Description { get; }
-
-        /// <summary>
-        ///     Gets a value indicating if any <see cref="IDataSourceGroup" /> processed with this processing mode
-        ///     can be combined with other processed <see cref="IDataSourceGroup" />s after processing is complete. Here,
-        ///     "combined" refers to correlating <see cref="IDataSource" /> data through the <see cref="DataSourceInfo" />s
-        ///     returned by separate groups' <see cref="ICustomDataProcessor.ProcessAsync" /> calls.
-        /// </summary>
-        /// <example>
-        ///     If this processing mode corrupts time information, this value should be <c>false</c> since combining
-        ///     its output with another <see cref="IDataSourceGroup" />'s output would result in mis-correlated
-        ///     time information on the combined <see cref="IDataSourceGroup" />.
-        /// </example>
-        bool SupportsGroupCombining { get; }
     }
 }
