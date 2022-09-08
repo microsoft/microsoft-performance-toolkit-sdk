0db0bad678a38b4814fbaaaa069a27aea55f4734
Luke Bordonaro <lukebo@microsoft.com>
2022-09-08 13:04:48 -0700

    Add note about reference equality

--- src/Microsoft.Performance.SDK/Processing/DataSourceGrouping/IDataSourceGrouper.cs
+++ src/Microsoft.Performance.SDK/Processing/DataSourceGrouping/IDataSourceGrouper.cs
@@ -21,13 +21,18 @@ namespace Microsoft.Performance.SDK.Processing.DataSourceGrouping
         /// <param name="dataSources">
         ///     The <see cref="IDataSource"/> instances to be grouped. This will be every opened <see cref="IDataSource"/>
         ///     that the implementing <see cref="IProcessingSource"/> specified it can open.
+        ///     <para />
+        ///     Every <see cref="IDataSource"/> inside a returned <see cref="IDataSourceGroup"/>'s
+        ///     <see cref="IDataSourceGroup.DataSources"/> property must be one of these instances.
         /// </param>
         /// <param name="options">
         ///     Options that will be passed to the implementing <see cref="IProcessingSource"/> for processing. This
         ///     can be used, for instance, to limit the types of <see cref="IDataSourceGroup"/>s that will be returned.
         /// </param>
         /// <returns>
-        ///     All valid groups of <see cref="IDataSource"/>s given the specified <paramref name="options"/>.
+        ///     All valid groups of <see cref="IDataSource"/>s given the specified <paramref name="options"/>. Every
+        ///     <see cref="IDataSource"/> inside a returned <see cref="IDataSourceGroup"/>'s <see cref="IDataSourceGroup.DataSources"/>
+        ///     property must be one of these instances.
         /// </returns>
         IReadOnlyCollection<IDataSourceGroup> Group(IEnumerable<IDataSource> dataSources, ProcessorOptions options);
     }
