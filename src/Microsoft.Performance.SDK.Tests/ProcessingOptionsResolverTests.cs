fcc77a4e1853f457274cd424d1055456b15afed5
Angeeras Ramanath <anramanath@microsoft.com>
2022-09-27 11:29:09 -0700

    Add file for unit tests.

---
+++ src/Microsoft.Performance.SDK.Tests/ProcessingOptionsResolverTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft.Performance.SDK.Tests
+{
+    internal class ProcessingOptionsResolverTests
+    {
+    }
+}
