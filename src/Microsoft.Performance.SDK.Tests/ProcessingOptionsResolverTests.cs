e878259f161689dcc189c05a600b7ff4ec0f1270
Angeeras Ramanath <anramanath@microsoft.com>
2022-09-28 10:08:13 -0700

    create test class.

--- src/Microsoft.Performance.SDK.Tests/ProcessingOptionsResolverTests.cs
+++ src/Microsoft.Performance.SDK.Tests/ProcessingOptionsResolverTests.cs
@@ -1,10 +1,54 @@
-﻿using System;
+﻿using Microsoft.Performance.SDK.Processing;
+using Microsoft.Performance.Testing;
+using Microsoft.Performance.Toolkit.Engine;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
-using System.Text;
 
 namespace Microsoft.Performance.SDK.Tests
 {
-    internal class ProcessingOptionsResolverTests
+    [TestClass]
+    public class ProcessingOptionsResolverTests
     {
+        [TestMethod]
+        [UnitTest]
+        public void ProcessingOptionsReferenceTest()
+        {
+            ProcessingOptionsResolver sut = new ProcessingOptionsResolver();
+
+            Assert.IsNotNull(sut);
+            Assert.IsNotNull(sut.OptionsForDataSources);
+            Assert.AreEqual(ProcessorOptions.Default, sut.GetProcessorOptions(null, null));
+        }
+
+        [TestMethod]
+        [UnitTest]
+        public void ProcessingOptionsResolverTests2()
+        {
+
+            //IDataSource dataSource = new FileDataSource();
+            //IDataSource dataSource2 = new FileDataSource();
+
+
+            var dataSourceOptions = new Dictionary<IEnumerable<IDataSource>, ProcessorOptions>()
+            {
+
+            };
+
+            ProcessingOptionsResolver sut = new ProcessingOptionsResolver(dataSourceOptions);
+
+            Assert.IsNotNull(sut);
+            Assert.IsNotNull(sut.OptionsForDataSources);
+
+
+            foreach (IEnumerable<IDataSource> dataSourceGroup in dataSourceOptions.Keys)
+            {
+
+
+                // Assert.AreEqual(dataSourceGroup, sut.OptionsForDataSources[dataSourceGroup]);
+            }
+
+
+        }
+
     }
 }
