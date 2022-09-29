fd31ac18ecfd9813b8874f9126440f77c60b8051
Angeeras Ramanath <anramanath@microsoft.com>
2022-09-29 10:30:31 -0700

    Move POR tests.

--- src/Microsoft.Performance.Toolkit.Engine.Tests/ProcessingOptionsResolverTests.cs
+++ src/Microsoft.Performance.Toolkit.Engine.Tests/ProcessingOptionsResolverTests.cs
@@ -1,13 +1,15 @@
 ﻿using Microsoft.Performance.SDK.Processing;
 using Microsoft.Performance.Testing;
 using Microsoft.Performance.Toolkit.Engine;
+using Microsoft.Performance.Toolkit.Engine.Tests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Collections.Generic;
 
 namespace Microsoft.Performance.SDK.Tests
 {
     [TestClass]
-    public class ProcessingOptionsResolverTests
+    public class ProcessingOptionsResolverTests 
+        : EngineFixture
     {
         [TestMethod]
         [UnitTest]
@@ -25,13 +27,17 @@ namespace Microsoft.Performance.SDK.Tests
         public void ProcessingOptionsResolverTests2()
         {
 
-            //IDataSource dataSource = new FileDataSource();
-            //IDataSource dataSource2 = new FileDataSource();
+            IEnumerable<IDataSource> dataSources = new List<IDataSource>()
+            {
+                new FileDataSource("test.txt"),
+                new FileDataSource("test2.txt")
+            };
 
+            var expectedProcessorOptions = new ProcessorOptions( new List<OptionInstance> {  } , new List<string>{ "arg1", "arg2" });
 
             var dataSourceOptions = new Dictionary<IEnumerable<IDataSource>, ProcessorOptions>()
             {
-
+                {  dataSources, ProcessorOptions.Default }
             };
 
             ProcessingOptionsResolver sut = new ProcessingOptionsResolver(dataSourceOptions);
