4592dc698d44916917d2a71defa24f4dde196f2f
helenkzhang <108310161+helenkzhang@users.noreply.github.com>
2022-12-21 17:52:43 -0800

    Add Plugin Metadata (#228)
    * Add  plugin manager core project
    
    * Add packaging metadata and test
    
    * Add target platforms and update supported data sources
    
    * Clean up
    
    * Clean up doc strings
    
    * More clean ups
    
    * Update .editorconfig
    
    * Update namespace
    
    * Add authors
    
    * Seal
    
    * Update test code
    
    * Remove .editorconfig
    
    * Remove camel case
    
    * Remove architecture and add IsMetadatatTable
    
    * Address comments
    
    * Remove group and upate test
    
    * Add data cookers and extensible tables

---
+++ src/PluginManager/Microsoft.Performance.Toolkit.PluginManager.Core/Packaging/Metadata/PluginMetadata.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.Performance.Toolkit.PluginManager.Core.Packaging.Metadata
+{
+    /// <summary>
+    /// Represents the metadata of a plugin
+    /// </summary>
+    public sealed class PluginMetadata
+    {
+        /// <summary>
+        /// The identifer of this plugin
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// The version of the plugin
+        /// </summary>
+        public Version Version { get; set; }
+
+        /// <summary>
+        /// The human-readable name of this plugin
+        /// </summary>
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// A user friendly description of this plugin
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The owners of this plugin
+        /// </summary>
+        public IEnumerable<PluginOwner> Owners { get; set; }
+
+        /// <summary>
+        /// The version of the performance SDK which this plugin depends upon
+        /// </summary>
+        public Version SdkVersion { get; set; }
+
+        /// <summary>
+        /// The metadata of the processing sources contained in this plugin
+        /// </summary>
+        public IEnumerable<ProcessingSourceMetadata> ProcessingSources { get; set; }
+
+        /// <summary>
+        /// The metadata of the data cookers contained in this plugin
+        /// </summary>
+        public IEnumerable<DataCookerMetadata> DataCookers { get; set; }
+
+        /// <summary>
+        /// The metadata of the extensible tables contained in this plugin
+        /// </summary>
+        public IEnumerable<TableMetadata> ExtensibleTables { get; set; }
+    }
+}
---
+++ src/PluginManager/Microsoft.Performance.Toolkit.PluginManager.Core/Packaging/Metadata/PluginOwner.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System.Collections.Generic;
+
+namespace Microsoft.Performance.Toolkit.PluginManager.Core.Packaging.Metadata
+{
+    /// <summary>
+    /// The information of a plugin owner
+    /// </summary>
+    public sealed class PluginOwner
+    {
+        /// <summary>
+        /// The name of the plugin owner
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The address of the owner, if any
+        /// </summary>
+        public string Address { get; set; }
+
+        /// <summary>
+        /// The email addresses of the owner, if any
+        /// </summary>
+        public IEnumerable<string> EmailAddresses { get; set; }
+
+        /// <summary>
+        /// The phone numbers of the owner, if any
+        /// </summary>
+        public IEnumerable<string> PhoneNumbers { get; set; }
+    }
+}
