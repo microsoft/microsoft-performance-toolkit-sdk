36a23348177e50177e33afe19e446836de0c06e3
helenkzhang <108310161+helenkzhang@users.noreply.github.com>
2022-12-21 17:52:43 -0800

    Add Plugin Metadata (#228)
    * Add  plugin manager core project
    
    * Add packaging metadata and test
    
    * Add target platforms and update supported data sources
    
    * Clean up
    
    * Clean up doc strings
    
    * More clean ups
    
    * Update .editorconfig
    
    * Update namespace
    
    * Add authors
    
    * Seal
    
    * Update test code
    
    * Remove .editorconfig
    
    * Remove camel case
    
    * Remove architecture and add IsMetadatatTable
    
    * Address comments
    
    * Remove group and upate test
    
    * Add data cookers and extensible tables

---
+++ src/PluginManager/Microsoft.Performance.Toolkit.PluginManager.Core/Packaging/Metadata/TableMetadata.cs
@@ -0,0 +1,38 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.Performance.Toolkit.PluginManager.Core.Packaging.Metadata
+{
+    /// <summary>
+    /// Represents the metadata of a table
+    /// </summary>
+    public sealed class TableMetadata
+    {
+        /// <summary>
+        /// The unique identifier of this table
+        /// </summary>
+        public Guid Guid { get; set; }
+
+        /// <summary>
+        /// The table name
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// A user friendly description of this table
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        /// The category into which this table belongs
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        /// Indicates whether this table is a metadata table
+        /// </summary>
+        public bool IsMetadataTable{ get; set; }
+    }
+}
---
+++ src/PluginManager/Microsoft.Performance.Toolkit.PluginManager.Core/PluginIdentity.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+
+namespace Microsoft.Performance.Toolkit.PluginManager.Core
+{
+    /// <summary>
+    /// Represents the core identity of a plugin
+    /// </summary>
+    public class PluginIdentity : IEquatable<PluginIdentity>
+    {
+        public PluginIdentity(string id, Version version)
+        {
+            this.Id = id;
+            this.Version = version;
+        }
+
+        /// <summary>
+        /// The identifer of this plugin
+        /// </summary>
+        public string Id { get; }
+
+        /// <summary>
+        /// The version of this plugin
+        /// </summary>
+        public Version Version { get; }
+
+        public bool Equals(PluginIdentity other)
+        {
+            return Equals(this, other);
+        }
+
+        public static bool Equals(PluginIdentity a, PluginIdentity b)
+        {
+            if (ReferenceEquals(a, b))
+            {
+                return true;
+            }
+
+            if (a is null || b is null)
+            {
+                return false;
+            }
+
+            return string.Equals(a.Id, b.Id, StringComparison.OrdinalIgnoreCase)
+                && Version.Equals(a.Version, b.Version);
+        }
+    }
+}
