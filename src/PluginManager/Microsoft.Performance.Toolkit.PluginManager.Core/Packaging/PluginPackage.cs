80762c7d8e5b7da962ffce1ed4bf7e8b1529e491
helenkzhang <108310161+helenkzhang@users.noreply.github.com>
2022-12-21 17:52:43 -0800

    Add Plugin Metadata (#228)
    * Add  plugin manager core project
    
    * Add packaging metadata and test
    
    * Add target platforms and update supported data sources
    
    * Clean up
    
    * Clean up doc strings
    
    * More clean ups
    
    * Update .editorconfig
    
    * Update namespace
    
    * Add authors
    
    * Seal
    
    * Update test code
    
    * Remove .editorconfig
    
    * Remove camel case
    
    * Remove architecture and add IsMetadatatTable
    
    * Address comments
    
    * Remove group and upate test
    
    * Add data cookers and extensible tables

---
+++ src/PluginManager/Microsoft.Performance.Toolkit.PluginManager.Core/Packaging/PluginPackage.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Performance.Toolkit.PluginManager.Core.Packaging
+{
+    /// <summary>
+    /// Represents a read-only plugin package
+    /// </summary>
+    public sealed class PluginPackage
+    {
+    }
+}
