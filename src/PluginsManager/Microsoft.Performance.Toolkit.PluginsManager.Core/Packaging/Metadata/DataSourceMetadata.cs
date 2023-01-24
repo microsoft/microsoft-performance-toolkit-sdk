7a938e4fc077607f7314f909732d059d93002fbc
Helen Zhang <helzhang@microsoft.com>
2023-01-24 00:21:52 -0800

    Update  metadata docstrings and add processing source guid in data cooker metadata

--- src/PluginsManager/Microsoft.Performance.Toolkit.PluginsManager.Core/Packaging/Metadata/DataSourceMetadata.cs
+++ src/PluginsManager/Microsoft.Performance.Toolkit.PluginsManager.Core/Packaging/Metadata/DataSourceMetadata.cs
@@ -6,12 +6,12 @@ using Microsoft.Performance.SDK.Processing;
 namespace Microsoft.Performance.Toolkit.PluginsManager.Core.Packaging.Metadata
 {
     /// <summary>
-    /// Represents the metadata of a supported data source
+    ///     Represents the metadata of a supported data source.
     /// </summary>
     public sealed class DataSourceMetadata
     {
         /// <summary>
-        /// The name of this data source
+        ///     Gets or sets the name of this data source.
         ///     If a <see cref="FileDataSource"/>, use the file extension as name (e.g. ".etl")
         ///     If a <see cref="DirectoryDataSource"/>, use "directory" as name
         ///     If a <see cref="ExtensionlessFileDataSourceAttribute"/>, use "extensionless" as name
@@ -19,7 +19,7 @@ namespace Microsoft.Performance.Toolkit.PluginsManager.Core.Packaging.Metadata
         public string Name { get; set; }
 
         /// <summary> 
-        /// The description of this data source
+        ///     Gets or sets the description of this data source.
         /// </summary>
         public string Description { get; set; }
     }
--- src/PluginsManager/Microsoft.Performance.Toolkit.PluginsManager.Core/Packaging/Metadata/PluginMetadata.cs
+++ src/PluginsManager/Microsoft.Performance.Toolkit.PluginsManager.Core/Packaging/Metadata/PluginMetadata.cs
@@ -7,52 +7,52 @@ using System.Collections.Generic;
 namespace Microsoft.Performance.Toolkit.PluginsManager.Core.Packaging.Metadata
 {
     /// <summary>
-    /// Represents the metadata of a plugin
+    ///     Represents the metadata of a plugin.
     /// </summary>
     public sealed class PluginMetadata
     {
         /// <summary>
-        /// The identifer of this plugin
+        ///     Gets or sets the identifer of this plugin.
         /// </summary>
         public string Id { get; set; }
 
         /// <summary>
-        /// The version of this plugin
+        ///     Gets or sets the version of this plugin.
         /// </summary>
         public Version Version { get; set; }
 
         /// <summary>
-        /// The human-readable name of this plugin
+        ///     Gets or sets the human-readable name of this plugin.
         /// </summary>
         public string DisplayName { get; set; }
 
         /// <summary>
-        /// A user friendly description of this plugin
+        ///     Gets or sets the user friendly description of this plugin.
         /// </summary>
         public string Description { get; set; }
 
         /// <summary>
-        /// The owners of this plugin
+        ///     Gets or sets the owners of this plugin.
         /// </summary>
         public IEnumerable<PluginOwner> Owners { get; set; }
 
         /// <summary>
-        /// The version of the performance SDK which this plugin depends upon
+        ///     Gets or sets the version of the performance SDK which this plugin depends upon.
         /// </summary>
         public Version SdkVersion { get; set; }
 
         /// <summary>
-        /// The metadata of the processing sources contained in this plugin
+        ///     Gets or sets the metadata of the processing sources contained in this plugin.
         /// </summary>
         public IEnumerable<ProcessingSourceMetadata> ProcessingSources { get; set; }
 
         /// <summary>
-        /// The metadata of the data cookers contained in this plugin
+        ///     Gets or sets the metadata of the data cookers contained in this plugin.
         /// </summary>
         public IEnumerable<DataCookerMetadata> DataCookers { get; set; }
 
         /// <summary>
-        /// The metadata of the extensible tables contained in this plugin
+        ///     Gets or sets the metadata of the extensible tables contained in this plugin.
         /// </summary>
         public IEnumerable<TableMetadata> ExtensibleTables { get; set; }
     }
