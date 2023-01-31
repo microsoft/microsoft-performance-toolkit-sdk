7923bfa32dac01e2187fd3c9afbc2053089fcfa3
Helen Zhang <helzhang@microsoft.com>
2023-01-30 17:27:08 -0800

    Add hash method

---
+++ src/PluginsManager/Microsoft.Performance.Toolkit.PluginsManager.Core/Utils/HashUtils.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Performance.SDK;
+
+namespace Microsoft.Performance.Toolkit.PluginsManager.Core.Utils
+{
+    /// <summary>
+    ///     Provides static methods for calculating hash.
+    /// </summary>
+    public static class HashUtils
+    {
+        /// <summary>
+        ///     Calculates the hash of all files under the given directory including the file paths.
+        /// </summary>
+        /// <param name="directory">
+        ///     Path to the directory.
+        /// </param>
+        /// <returns>
+        ///     The combined MD5 hash of all files including the file paths.
+        /// </returns>
+        public static async Task<string> GetDirectoryHash(string directory)
+        {
+            Guard.NotNull(directory, nameof(directory));
+            Guard.IsTrue(Directory.Exists(directory), $"Directory {directory} doesn't exist.");
+
+            using (var md5 = MD5.Create())
+            using (var cs = new CryptoStream(Stream.Null, md5, CryptoStreamMode.Write))
+            {
+                foreach (string filePath in Directory.EnumerateFiles(directory))
+                {
+                    // Hash file paths
+                    byte[] pathBytes = Encoding.UTF8.GetBytes(filePath);
+                    cs.Write(pathBytes, 0, pathBytes.Length);
+
+                    // Hash file content
+                    using (FileStream stream = File.OpenRead(filePath))
+                    {
+                        await stream.CopyToAsync(cs);
+                    }
+                }
+
+                cs.FlushFinalBlock();
+                return BitConverter.ToString(md5.Hash).Replace("-", "").ToLower();
+            }
+        }
+    }
+}
