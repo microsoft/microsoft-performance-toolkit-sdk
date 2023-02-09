eefc0ca2c880a5e68fa822237d17e6dd7069e03d
helenkzhang <108310161+helenkzhang@users.noreply.github.com>
2023-02-09 11:42:10 -0800

    Plugins Installation (#239)
    * Update  metadata docstrings and add processing source guid in data cooker metadata
    
    * Checkpoint
    
    * Add installation APIs
    
    * Add lock file apis
    
    * Add supports for getting installed plugin metadata
    
    * Implements lock
    
    * Add progress to package extraction
    
    * Implements verify installed
    
    * Checks for installation and refactor synchronized object
    
    * Add hash method
    
    * Add hash in InstalledPlugin
    
    * Add obsolete plugin files clean up
    
    * Remove tests csproj
    
    * Add TODO
    
    * Move InstalledPlugin to Installation
    
    * Add plugin package docstrings
    
    * Tag todos with issue
    
    * Address comment
    
    * Update installer to use new AvailablePlugin
    
    * Fix name
    
    * Add awaits
    
    * Address checksum comments
    
    * Get rid of update plugin
    
    * Deserialize metadata on getting installed plugins
    
    * Clean ups

--- src/PluginsManager/Microsoft.Performance.Toolkit.PluginsManager.Core/Packaging/Metadata/PluginMetadata.cs
+++ src/PluginsManager/Microsoft.Performance.Toolkit.PluginsManager.Core/Packaging/Metadata/PluginMetadata.cs
@@ -3,56 +3,71 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Threading.Tasks;
+using Microsoft.Performance.SDK;
 
 namespace Microsoft.Performance.Toolkit.PluginsManager.Core.Packaging.Metadata
 {
     /// <summary>
-    /// Represents the metadata of a plugin
+    ///     Represents the metadata of a plugin.
     /// </summary>
     public sealed class PluginMetadata
     {
+        public static async Task<PluginMetadata> Parse(Stream jsonStream)
+        {
+            Guard.NotNull(jsonStream, nameof(jsonStream));
+
+            // TODO: #238 Error handling
+            using (var doc = JsonDocument.Parse(jsonStream))
+            {
+                return await JsonSerializer.DeserializeAsync<PluginMetadata>(jsonStream);
+            }
+        }
+
         /// <summary>
-        /// The identifer of this plugin
+        ///     Gets or sets the identifer of this plugin.
         /// </summary>
         public string Id { get; set; }
 
         /// <summary>
-        /// The version of this plugin
+        ///     Gets or sets the version of this plugin.
         /// </summary>
         public Version Version { get; set; }
 
         /// <summary>
-        /// The human-readable name of this plugin
+        ///     Gets or sets the human-readable name of this plugin.
         /// </summary>
         public string DisplayName { get; set; }
 
         /// <summary>
-        /// A user friendly description of this plugin
+        ///     Gets or sets the user friendly description of this plugin.
         /// </summary>
         public string Description { get; set; }
 
         /// <summary>
-        /// The owners of this plugin
+        ///     Gets or sets the owners of this plugin.
         /// </summary>
         public IEnumerable<PluginOwner> Owners { get; set; }
 
         /// <summary>
-        /// The version of the performance SDK which this plugin depends upon
+        ///     Gets or sets the version of the performance SDK which this plugin depends upon.
         /// </summary>
         public Version SdkVersion { get; set; }
 
         /// <summary>
-        /// The metadata of the processing sources contained in this plugin
+        ///     Gets or sets the metadata of the processing sources contained in this plugin.
         /// </summary>
         public IEnumerable<ProcessingSourceMetadata> ProcessingSources { get; set; }
 
         /// <summary>
-        /// The metadata of the data cookers contained in this plugin
+        ///     Gets or sets the metadata of the data cookers contained in this plugin.
         /// </summary>
         public IEnumerable<DataCookerMetadata> DataCookers { get; set; }
 
         /// <summary>
-        /// The metadata of the extensible tables contained in this plugin
+        ///     Gets or sets the metadata of the extensible tables contained in this plugin.
         /// </summary>
         public IEnumerable<TableMetadata> ExtensibleTables { get; set; }
     }
