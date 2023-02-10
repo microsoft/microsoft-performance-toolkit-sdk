1b3cf42b17ba3864ca90df37074d881a09e9b60f
helenkzhang <108310161+helenkzhang@users.noreply.github.com>
2023-02-10 09:19:44 -0800

    Update project names (#262)
    * Update project names and dirs
    
    * Update namespaces

---
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Core/Credential/ICredentials.cs
@@ -0,0 +1,11 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Performance.Toolkit.Plugins.Core.Credential
+{
+
+    // TODO: #235 Authentication
+    public interface ICredentials
+    {
+    }
+}
