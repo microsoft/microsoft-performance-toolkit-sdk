afb39a6aeeda264e195ae0e8c1a646d9bc6cd039
helenkzhang <108310161+helenkzhang@users.noreply.github.com>
2023-02-13 15:34:33 -0800

    Add runtime project (#265)
    * Add runtime
    
    * Add folders
    
    * Add tests projects
    
    * Remove global usings
    
    * Fix path

---
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Publisher.Cli/Program.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Performance.Toolkit.Plugins.Publisher.Cli
+{
+    public sealed class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
