e01ac30ac8b6ce38d03cebc5cb46a70bdfea8a56
Helen Zhang <helzhang@microsoft.com>
2023-02-16 10:46:06 -0800

    Add resource loader implementation with refactorings

--- src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Extensibility/IPluginManagerResourceRepository.cs
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Extensibility/IPluginManagerResourceRepository.cs
@@ -8,26 +8,18 @@ using Microsoft.Performance.Toolkit.Plugins.Core.Extensibility;
 namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Extensibility
 {
     /// <summary>
-    ///     Represents a repository for storing a collection of <see cref=" IPluginManagerResource"/>.
+    ///     Represents a repository for storing a collection of <see cref=" IPluginManagerResource"/> of type <typeparamref name="T"/>.
     /// </summary>
     /// <typeparam name="T">
     ///     The type of the resources stored in this repository.
     /// </typeparam>
     public interface IPluginManagerResourceRepository<T>
-        where T : class, IPluginManagerResource
+        where T : IPluginManagerResource
     {
         /// <summary>
-        ///     Gets all plugin resources contained in this repository.
+        ///     Gets all plugins manager resources contained in this repository.
         /// </summary>
-        IEnumerable<T> PluginResources { get; }
-
-        /// <summary>
-        ///     Adds new resources and notifies when they are added to the repository.
-        /// </summary>
-        /// <param name="loadedResources">
-        ///     The newly loaded resources.
-        /// </param>
-        void OnNewResourcesLoaded(IEnumerable<T> loadedResources);
+        IEnumerable<T> Resources { get; }
 
         /// <summary>
         ///    Raised when new resources are added to this repository.
