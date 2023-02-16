928f3aea58477f6468e5c1aa132c1151890eaaed
helenkzhang <108310161+helenkzhang@users.noreply.github.com>
2023-02-16 15:38:54 -0800

    Add support for plugins manager resources reflection and loading (#267)
    * Add logger in PluginsManager
    
    * Add plugin manager resource reference class
    
    * Add resource loader implementation with refactorings

---
+++ src/Microsoft.Performance.SDK.Runtime/PluginManagerResourceReference.cs
@@ -0,0 +1,142 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.Reflection;
+using Microsoft.Performance.SDK.Runtime;
+using Microsoft.Performance.Toolkit.Plugins.Core.Extensibility;
+
+namespace Microsoft.Performance.SDK.Runtime
+{
+    /// <summary>
+    ///     Creates a <see cref="AssemblyTypeReferenceWithInstance{T, Derived}"/> 
+    ///     where T is <see cref="IPluginManagerResource"/> and Derived is <see cref="PluginManagerResourceReference"/>.
+    /// </summary>
+    public sealed class PluginManagerResourceReference
+         : AssemblyTypeReferenceWithInstance<IPluginManagerResource, PluginManagerResourceReference>
+    {
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PluginManagerResourceReference"/> class.
+        /// </summary>
+        /// <param name="type">
+        ///     The <see cref="Type"/> of instance being referenced by this instance.
+        /// </param>
+        /// <param name="metadata">
+        ///     The <see cref="IPluginManagerResource"/> metadata.
+        /// </param>
+        public PluginManagerResourceReference(
+            Type type,
+            PluginManagerResourceAttribute metadata)
+            : base(type)
+        {
+            Guard.NotNull(type, nameof(type));
+            Guard.NotNull(metadata, nameof(metadata));
+
+            this.Guid = metadata.Guid;
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PluginManagerResourceReference"/>
+        ///     class with the given instance.
+        /// </summary>
+        /// <param name="instance">
+        ///     An existing instance of <see cref="IPluginManagerResource"/>
+        /// </param>
+        public PluginManagerResourceReference(IPluginManagerResource instance)
+            : base(instance.GetType(), () => instance)
+        {
+            Guard.NotNull(instance, nameof(instance));
+
+            this.Guid = instance.TryGetGuid();
+        }
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="PluginManagerResourceReference"/>
+        ///     class as a copy of the given instance.
+        /// </summary>
+        /// <param name="other">
+        ///      The instance from which to make a copy.
+        /// </param>
+        public PluginManagerResourceReference(PluginManagerResourceReference other)
+            : base(other.Type)
+        {
+            Guard.NotNull(other, nameof(other));
+
+            other.ThrowIfDisposed();
+
+            this.Guid = other.Guid;
+        }
+
+        /// <summary>
+        ///     Tries to create an instance of <see cref="PluginManagerResourceReference"/> 
+        ///     based on the <paramref name="candidateType"/>.
+        /// </summary>
+        /// <param name="candidateType">
+        ///      Candidate <see cref="Type"/> for the <see cref="PluginManagerResourceReference"/>
+        /// </param>
+        /// <param name="reference">
+        ///      Out <see cref="PluginManagerResourceReference"/>
+        /// </param>
+        /// <returns>
+        ///     <c>true</c> if the <paramref name="candidateType"/> is valid and can create a instance of 
+        ///     <see cref="PluginManagerResourceReference"/>; <c>false</c> otherwise.
+        /// </returns>
+        public static bool TryCreateReference(
+            Type candidateType,
+            out PluginManagerResourceReference reference)
+        {
+            Guard.NotNull(candidateType, nameof(candidateType));
+
+            reference = null;
+            if (IsValidType(candidateType))
+            {
+                if (candidateType.TryGetEmptyPublicConstructor(out ConstructorInfo constructor))
+                {
+                    PluginManagerResourceAttribute metadataAttribute = candidateType.GetCustomAttribute<PluginManagerResourceAttribute>();
+                    if (metadataAttribute != null)
+                    {
+                        reference = new PluginManagerResourceReference(
+                            candidateType,
+                            metadataAttribute);
+                    }
+                }
+            }
+
+            return reference != null;
+        }
+
+        /// <summary>
+        ///     Gets the Guid of this <see cref="IPluginManagerResource"/>.
+        /// </summary>
+        public Guid Guid { get; }
+
+        /// <inheritdoc />
+        public override PluginManagerResourceReference CloneT()
+        {
+            return new PluginManagerResourceReference(this);
+        }
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (obj is null)
+            {
+                return false;
+            }
+
+            if (ReferenceEquals(this, obj))
+            {
+                return true;
+            }
+
+            return obj is PluginManagerResourceReference other &&
+                this.Guid.Equals(other.Guid);
+        }
+
+        /// <inheritdoc />
+        public override int GetHashCode()
+        {
+            return this.Guid.GetHashCode();
+        }
+    }
+}
--- src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Extensibility/IPluginManagerResourceLoader.cs
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Extensibility/IPluginManagerResourceLoader.cs
@@ -7,12 +7,13 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Extensibility
 {
     /// <summary>
     ///     Represents a loader that facilitates the loading of <see cref="IPluginManagerResource"/>s.
+    ///     to its consumers.
     /// </summary>
     public interface IPluginManagerResourceLoader
     {
         /// <summary>
         ///     Tries to load <see cref="IPluginManagerResource"/>s from the given directory
-        ///     to the subscribed <see cref="IPluginManagerResourceRepository{T}">s.
+        ///     to the subscribed <see cref="IPluginManagerResourcesConsumer">s.
         /// <param name="directory">
         ///     The directoty to load resouces from.
         /// </param>
@@ -23,31 +24,27 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Extensibility
         bool TryLoad(string directory);
 
         /// <summary>
-        ///     Registers a <see cref="IPluginManagerResourceRepository{T}"/> to receive all future loaded resources.
+        ///     Registers a <see cref="IPluginManagerResourcesConsumer}"/> to receive all future loaded resources
+        ///     and sends all previously loaded plugins to its <see cref="IPluginManagerResourcesConsumer.OnNewResourcesLoaded(
+        ///     System.Collections.Generic.IEnumerable{SDK.Runtime.PluginManagerResourceReference})"/> handler.
         /// </summary>
-        /// <typeparam name="T">
-        ///     The type of the resources stored in the given <paramref name="resourceRepository"/>.
-        /// </typeparam>
-        /// <param name="resourceRepository">
-        ///     The <see cref="IPluginManagerResourceRepository{T}"/> to be registered to this loader.
+        /// <param name="consumer">
+        ///     The <see cref="IPluginManagerResourcesConsumer"/> to be registered to this loader.
         /// </param>
         /// <returns>
-        ///     Whether or not the <paramref name="resourceRepository"/> is successfully subscribed.
+        ///     Whether or not the <paramref name="consumer"/> is successfully subscribed.
         /// </returns>
-        bool Subscribe<T>(IPluginManagerResourceRepository<T> resourceRepository) where T : class, IPluginManagerResource;
+        bool Subscribe(IPluginManagerResourcesConsumer consumer);
 
         /// <summary>
-        ///     Unregisters a <see cref="IPluginManagerResourceRepository{T}"/> from receiving future loaded resources.
+        ///     Unregisters a <see cref="IPluginManagerResourcesConsumer"/> from receiving future loaded resources.
         /// </summary>
-        /// <typeparam name="T">
-        ///     The type of the resources stored in the given <paramref name="resourceRepository"/>.
-        /// </typeparam>
-        /// <param name="resourceRepository">
-        ///     The <see cref="IPluginManagerResourceRepository{T}"/> to be unregistered from this loader.
+        /// <param name="consumer">
+        ///     The <see cref="IPluginManagerResourcesConsumer"/> to be unregistered from this loader.
         /// </param>
         /// <returns>
-        ///     Whether or not the <paramref name="resourceRepository"/> is successfully unsubscribed.
+        ///     Whether or not the <paramref name="consumer"/> is successfully unsubscribed.
         /// </returns>
-        bool Unsubscribe<T>(IPluginManagerResourceRepository<T> resourceRepository) where T : class, IPluginManagerResource;
+        bool Unsubscribe(IPluginManagerResourcesConsumer consumer);
     }
 }
