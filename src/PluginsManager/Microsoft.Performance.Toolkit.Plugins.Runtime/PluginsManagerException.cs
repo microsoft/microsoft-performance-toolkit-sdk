1bf253785d52540e61db0ea8e23ba3e9317cfb04
Helen Zhang <helzhang@microsoft.com>
2023-02-24 09:04:26 -0800

    Update exception types

--- src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/PluginsManagerException.cs
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/PluginsManagerException.cs
@@ -21,8 +21,44 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime
         }
     }
 
-    public class PluginRegistryCorruptedException
+    //
+    // PluginRegistry Exception Types
+    //
+    
+    public class PluginRegistryException
         : PluginsManagerException
+    {
+        public PluginRegistryException()
+        {
+        }
+
+        public PluginRegistryException(string message)
+            : base(message)
+        {
+        }
+
+        public PluginRegistryException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public PluginRegistryException(string message, string registryFilePath)
+            : this(message)
+        {
+            this.PluginRegistryPath = registryFilePath;
+        }
+
+        public PluginRegistryException(string message, string registryFilePath, Exception innerException)
+            : this(message, innerException)
+        {
+            this.PluginRegistryPath = registryFilePath;
+        }
+
+        public string PluginRegistryPath { get; }
+    }
+
+    public class PluginRegistryCorruptedException
+        : PluginRegistryException
     {
         public PluginRegistryCorruptedException()
         {
@@ -39,16 +75,18 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime
         }
 
         public PluginRegistryCorruptedException(string message, string registryFilePath)
-            : this(message)
+            : base(message, registryFilePath)
         {
-            this.PluginRegistryPath = registryFilePath;
         }
 
-        public string PluginRegistryPath { get; }
+        public PluginRegistryCorruptedException(string message, string registryFilePath, Exception innerException)
+            : base(message, registryFilePath, innerException)
+        {
+        }
     }
 
     public class PluginRegistryReadWriteException
-        : PluginsManagerException
+        : PluginRegistryException
     {
         public PluginRegistryReadWriteException()
         {
@@ -64,17 +102,126 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime
         {
         }
 
-        public PluginRegistryReadWriteException(string message, string registryFilePath, Exception innerException)
-            : this(message, innerException)
+        public PluginRegistryReadWriteException(string message, string registryFilePath)
+            : base(message, registryFilePath)
         {
-            this.PluginRegistryPath = registryFilePath;
         }
 
-        public string PluginRegistryPath { get; }
+        public PluginRegistryReadWriteException(string message, string registryFilePath, Exception innerException)
+            : base(message, registryFilePath, innerException)
+        {
+        }
+    }
+
+
+    //
+    // PluginPackage Exception Types
+    //
+
+    public class PluginPackageException
+        : PluginsManagerException
+    {
+        public PluginPackageException()
+        {
+        }
+
+        public PluginPackageException(string message)
+            : base(message)
+        {
+        }
+
+        public PluginPackageException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    public class PluginPackageCreationException
+        : PluginPackageException
+    {
+        public PluginPackageCreationException()
+        {
+        }
+
+        public PluginPackageCreationException(string message)
+            : base(message)
+        {
+        }
+
+        public PluginPackageCreationException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    public class PluginPackageExtractionException
+        : PluginPackageException
+    {
+        public PluginPackageExtractionException()
+        {
+        }
+
+        public PluginPackageExtractionException(string message)
+            : base(message)
+        {
+        }
+
+        public PluginPackageExtractionException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    public class MalformedPluginPackageException
+        : PluginPackageException
+    {
+        public MalformedPluginPackageException()
+        {
+        }
+
+        public MalformedPluginPackageException(string message)
+            : base(message)
+        {
+        }
+
+        public MalformedPluginPackageException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+
+    //
+    // InstalledPlugin Exception Types
+    //
+
+    public class InstalledPluginException
+        : PluginsManagerException
+    {
+        public InstalledPluginException()
+        {
+        }
+
+        public InstalledPluginException(string message)
+            : base(message)
+        {
+        }
+
+        public InstalledPluginException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+
+        public InstalledPluginException(string message, InstalledPluginInfo pluginInfo)
+            : this(message)
+        {
+            this.PluginInfo = pluginInfo;
+        }
+
+        public InstalledPluginInfo PluginInfo { get; }
     }
 
     public class InstalledPluginCorruptedOrMissingException
-        : PluginsManagerException
+        : InstalledPluginException
     {
         public InstalledPluginCorruptedOrMissingException()
         {
@@ -98,60 +245,10 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime
 
         public InstalledPluginInfo PluginInfo { get; }
     }
-    
-    public class PluginPackageCreationException
-        : PluginsManagerException
-    {
-        public PluginPackageCreationException()
-        {
-        }
 
-        public PluginPackageCreationException(string message)
-            : base(message)
-        {
-        }
-
-        public PluginPackageCreationException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-    }
-
-    public class PluginPackageExtractionException
-        : PluginsManagerException
-    {
-        public PluginPackageExtractionException()
-        {
-        }
-
-        public PluginPackageExtractionException(string message)
-            : base(message)
-        {
-        }
-
-        public PluginPackageExtractionException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-    }
-
-    public class MalformedPluginPackageException
-        : PluginsManagerException
-    {
-        public MalformedPluginPackageException()
-        {
-        }
-
-        public MalformedPluginPackageException(string message)
-            : base(message)
-        {
-        }
-
-        public MalformedPluginPackageException(string message, Exception innerException)
-            : base(message, innerException)
-        {
-        }
-    }
+    //
+    // Fetchinf Exception Types
+    //
 
     public class PluginFetchingException
         : PluginsManagerException
