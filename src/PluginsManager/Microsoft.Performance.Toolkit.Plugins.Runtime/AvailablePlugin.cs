0df7ea8d81f5a226d7a2114918413541921d11ab
Helen Zhang <helzhang@microsoft.com>
2023-03-03 11:41:34 -0800

    Clean up and fix white spaces

--- src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/AvailablePlugin.cs
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/AvailablePlugin.cs
@@ -5,7 +5,6 @@ using System;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.Performance.SDK.Runtime;
 using Microsoft.Performance.Toolkit.Plugins.Core;
 using Microsoft.Performance.Toolkit.Plugins.Core.Discovery;
 using Microsoft.Performance.Toolkit.Plugins.Core.Packaging.Metadata;
