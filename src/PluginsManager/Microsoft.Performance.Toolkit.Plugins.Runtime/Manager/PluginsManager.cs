c49b96109934b2f09337f030978be0f33041c62c
Helen Zhang <helzhang@microsoft.com>
2023-03-05 23:51:25 -0800

    Fix AddPluginSources

--- src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Manager/PluginsManager.cs
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Manager/PluginsManager.cs
@@ -122,9 +122,9 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Manager
         }
 
         /// <inheritdoc />
-        public void AddPluginSources(IEnumerable<PluginSource> sources)
+        public async Task AddPluginSources(IEnumerable<PluginSource> sources)
         {
-            this.discovererSourcesManager.AddPluginSources(sources);
+            await this.discovererSourcesManager.AddPluginSources(sources);
         }
 
         // TODO: Re-enable when we start to support loading additional resources.
