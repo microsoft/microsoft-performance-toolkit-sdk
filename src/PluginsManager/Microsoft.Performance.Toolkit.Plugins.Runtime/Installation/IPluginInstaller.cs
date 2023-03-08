869ccd5095e4f0a0d8613299311aacd9473db508
Helen Zhang <helzhang@microsoft.com>
2023-03-08 12:14:07 -0800

    Add IPluginInstaller

---
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Installation/IPluginInstaller.cs
@@ -0,0 +1,107 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Installation
+{
+    /// <summary>
+    ///     Represents an installer that supports listing, installing, uninstalling and cleaning up plugins.
+    /// </summary>
+    public interface IPluginInstaller
+    {
+        /// <summary>
+        ///     Gets all installed plugins.
+        /// </summary>
+        /// <param name="cancellationToken">
+        ///     Signals that the caller wishes to cancel the operation.
+        /// </param>
+        /// <returns>
+        ///     A <see cref="InstalledPluginsResults"/> instance containing all valid and invalid installed plugins.
+        /// </returns>
+        /// <exception cref="OperationCanceledException">
+        ///     Throws when the operation was cancelled.
+        /// </exception>
+        Task<InstalledPluginsResults> GetAllInstalledPluginsAsync(
+            CancellationToken cancellationToken);
+
+        /// <summary>
+        ///     Installs a plugin from a stream.
+        /// </summary>
+        /// <param name="pluginStream">
+        ///     A stream containing the plugin to install.
+        /// </param>
+        /// <param name="installationRoot">
+        ///     The root directory where the plugin will be installed.
+        /// </param>
+        /// <param name="sourceUri">
+        ///     The URI of the <see cref="Core.Discovery.PluginSource"/> that the plugin was discovered from.
+        /// </param>
+        /// <param name="cancellationToken">
+        ///     Indicates the progress of the installation.
+        /// </param>
+        /// <param name="progress">
+        ///     Indicates the progress of the installation.
+        /// </param>
+        /// <returns>
+        ///     The <see cref="InstalledPluginInfo"/> of the installed plugin if the installation was successful. Otherwise, null.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///      Throws when <paramref name="pluginPackage"/> or <paramref name="installationRoot"/> or
+        ///      <paramref name="sourceUri"/> is null.
+        /// </exception>
+        /// <exception cref="OperationCanceledException">
+        ///     Throws when the operation was cancelled.
+        /// </exception>
+        Task<InstalledPluginInfo> InstallPluginAsync(
+            Stream pluginStream,
+            string installationRoot,
+            Uri sourceUri,
+            CancellationToken cancellationToken,
+            IProgress<int> progress);
+
+        /// <summary>
+        ///     Uninstalls a plugin.
+        /// </summary>
+        /// <param name="installedPlugin">
+        ///     The plugin to uninstall.
+        /// </param>
+        /// <param name="cancellationToken">
+        ///     Signals that the caller wishes to cancel the operation.
+        /// </param>
+        /// <returns>
+        ///     True if the uninstallation was successful. Otherwise, false.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">
+        ///     Throws when <paramref name="installedPlugin"/> is null.
+        /// </exception>
+        /// <exception cref="OperationCanceledException">
+        ///     Throws when the operation was cancelled.
+        /// </exception>
+        Task<bool> UninstallPluginAsync(
+            InstalledPlugin installedPlugin,
+            CancellationToken cancellationToken);
+
+        /// <summary>
+        ///     Attempts to clean up all obsolete (unreigstered) plugin files from the given installation directory.
+        /// </summary>
+        /// <param name="installationDir">
+        ///     The installation directory to clean up.
+        /// </param>
+        /// <param name="cancellationToken">
+        ///     Signals that the caller wishes to cancel the operation.
+        /// </param>
+        /// <returns>
+        ///     A task that completes when the cleanup is complete.
+        /// </returns>
+        /// <exception cref="OperationCanceledException">
+        ///     Throws when the operation was canceled.
+        /// </exception>
+        Task CleanupObsoletePluginsAsync(
+            string installationDir,
+            CancellationToken cancellationToken);
+    }
+}
