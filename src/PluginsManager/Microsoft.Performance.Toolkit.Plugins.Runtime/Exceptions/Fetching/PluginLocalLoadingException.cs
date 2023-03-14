bb359bffb221c02007af257fcfaa110cc3b9adb5
Helen Zhang <helzhang@microsoft.com>
2023-03-14 12:42:56 -0700

    Update exceptions

---
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Exceptions/Fetching/PluginLocalLoadingException.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+
+namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Exceptions
+{
+    /// <summary>
+    ///    Represents an exception that occurs when a plugin package cannot be loaded.
+    /// </summary>
+    public class PluginLocalLoadingException
+        : PluginsManagerException
+    {
+        public PluginLocalLoadingException()
+        {
+        }
+
+        public PluginLocalLoadingException(string message)
+            : base(message)
+        {
+        }
+
+        public PluginLocalLoadingException(string message, Exception innerException)
+            : base(message, innerException)
+        {
+        }
+    }
+}
