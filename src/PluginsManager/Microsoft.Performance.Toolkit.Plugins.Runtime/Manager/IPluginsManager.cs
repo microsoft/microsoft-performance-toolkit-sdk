2fc829264fcf2a27e51e98b21e13dd548798bb82
helenkzhang <108310161+helenkzhang@users.noreply.github.com>
2023-03-15 15:47:29 -0700

    Remove installer APIs from IPluginsManager (#279)
    * Remove installer APIs from plugins manager and just make it a facade of IPluginsInstaller
    
    * Change installer class name
    
    * Docstrings

--- src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Manager/IPluginsManager.cs
+++ src/PluginsManager/Microsoft.Performance.Toolkit.Plugins.Runtime/Manager/IPluginsManager.cs
@@ -8,7 +8,6 @@ using System.Threading.Tasks;
 using Microsoft.Performance.Toolkit.Plugins.Core;
 using Microsoft.Performance.Toolkit.Plugins.Core.Discovery;
 using Microsoft.Performance.Toolkit.Plugins.Runtime.Events;
-using Microsoft.Performance.Toolkit.Plugins.Runtime.Exceptions;
 using Microsoft.Performance.Toolkit.Plugins.Runtime.Installation;
 
 namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Manager
@@ -17,6 +16,7 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Manager
     ///     Contains logic for discovering, installing, uninstalling and updating plugins.
     /// </summary>
     public interface IPluginsManager
+        : IPluginsInstaller
     {
         /// <summary>
         ///     Gets all plugin sources managed by this plugins manager.
@@ -113,154 +113,6 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Manager
             PluginSource pluginSource,
             PluginIdentity pluginIdentity,
             CancellationToken cancellationToken);
-
-        /// <summary>
-        ///     Gets all installed plugins
-        /// </summary>
-        /// <param name="cancellationToken">
-        ///     Signals that the caller wishes to cancel the operation.
-        /// </param>
-        /// <returns>
-        ///     A <see cref="InstalledPluginsResults"/> instance containing all valid and invalid installed plugins.
-        /// </returns>
-        /// <exception cref="RepositoryDataAccessException">
-        ///     Throws when there is an error reading or writing to plugin registry.
-        /// </exception>
-        /// <exception cref="RepositoryCorruptedException">
-        ///     Throws when the plugin registry is in an invalid state.
-        /// </exception>
-        /// <exception cref="OperationCanceledException">
-        ///     The operation was canceled.
-        /// </exception>
-        Task<InstalledPluginsResults> GetInstalledPluginsAsync(CancellationToken cancellationToken);
-
-        /// <summary>
-        ///     Installs an available plugin if no other versions of this plugin installed.
-        /// </summary>
-        /// <param name="availablePlugin">
-        ///     The available plugin to be installed.
-        /// </param>
-        /// <param name="cancellationToken">
-        ///     Signals that the caller wishes to cancel the operation.
-        /// </param>
-        /// <param name="progress">
-        ///     Indicates the progress of plugin installation.
-        /// </param>
-        /// <returns>
-        ///     The <see cref="InstalledPluginInfo"/> if plugin is successfully installed. <c>null</c> otherwise.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///     Throws when <paramref name="availablePlugin"/> is <c>null</c>.
-        /// </exception>
-        /// <exception cref="PluginFetchingException">
-        ///     Throws when the plugin package cannot be fetched.
-        /// </exception>
-        /// <exception cref="RepositoryDataAccessException">
-        ///     Throws when there is an error reading or writing to plugin registry.
-        /// </exception>
-        /// <exception cref="RepositoryCorruptedException">
-        ///     Throws when the plugin registry is in an invalid state.
-        /// </exception>
-        /// <exception cref="PluginPackageCreationException">
-        ///     Throws when there is an error creating plugin package.
-        /// </exception>
-        /// <exception cref="PluginPackageExtractionException">
-        ///     Throws when there is an error extracting plugin package.
-        /// </exception>
-        /// <exception cref="OperationCanceledException">
-        ///     Throws when the operation was canceled.
-        /// </exception>
-        Task<InstalledPluginInfo> TryInstallAvailablePluginAsync(
-            AvailablePlugin availablePlugin,
-            CancellationToken cancellationToken,
-            IProgress<int> progress);
-
-        /// <summary>
-        ///     Installs a local plugin package.
-        /// </summary>
-        /// <param name="pluginPackagePath">
-        ///     The path to the plugin package to be installed.
-        /// </param>
-        /// <param name="cancellationToken">
-        ///     Signals that the caller wishes to cancel the operation.
-        /// </param>
-        /// <param name="progress">
-        ///     Indicates the progress of plugin installation.
-        /// </param>
-        /// <returns>
-        ///     The <see cref="InstalledPluginInfo"/> if plugin is successfully installed.. <c>null</c> otherwise.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///     Throws when <paramref name="pluginPackagePath"/> is <c>null</c>.
-        /// </exception>
-        /// <exception cref="RepositoryDataAccessException">
-        ///     Throws when there is an error reading or writing to plugin registry.
-        /// </exception>
-        /// <exception cref="RepositoryCorruptedException">
-        ///     Throws when the plugin registry is in an invalid state.
-        /// </exception>
-        /// <exception cref="PluginPackageCreationException">
-        ///     Throws when there is an error creating plugin package.
-        /// </exception>
-        /// <exception cref="PluginPackageExtractionException">
-        ///     Throws when there is an error extracting plugin package.
-        /// </exception>
-        /// <exception cref="OperationCanceledException">
-        ///     Throws when the operation was canceled.
-        /// </exception>
-        Task<InstalledPluginInfo> TryInstallLocalPluginAsync(
-           string pluginPackagePath,
-           CancellationToken cancellationToken,
-           IProgress<int> progress);
-
-        /// <summary>
-        ///     Uninstalls a plugin.
-        /// </summary>
-        /// <param name="installedPlugin">
-        ///     The plugin to be uninstalled.
-        /// </param>
-        /// <param name="cancellationToken">
-        ///     Signals that the caller wishes to cancel the operation.
-        /// </param>
-        /// <returns>
-        ///     <c>true</c> if the plugin has been successfully uninstalled. <c>false</c> otherwise.
-        /// </returns>
-        /// <exception cref="ArgumentNullException">
-        ///     Throws when <paramref name="installedPlugin"/> is <c>null</c>.
-        /// </exception>
-        /// <exception cref="RepositoryDataAccessException">
-        ///     Throws when there is an error reading or writing to plugin registry.
-        /// </exception>
-        /// <exception cref="RepositoryCorruptedException">
-        ///     Throws when the plugin registry is in an invalid state.
-        /// </exception>
-        /// <exception cref="OperationCanceledException">
-        ///     Throws when the operation was canceled.
-        /// </exception>
-        Task<bool> UninstallPluginAsync(
-            InstalledPlugin installedPlugin,
-            CancellationToken cancellationToken);
-
-        /// <summary>
-        ///     Attempts to clean up all obsolete (unreigstered) plugin files.
-        ///     This method should be called safely by plugins consumers.
-        /// </summary>
-        /// <param name="cancellationToken">
-        ///     Signals that the caller wishes to cancel the operation.
-        /// </param>
-        /// <returns>
-        ///     An await-able <see cref="Task"/> that, upon completion, indicates the files have been cleaned up.
-        /// </returns>
-        /// <exception cref="RepositoryDataAccessException">
-        ///     Throws when there is an error reading or writing to plugin registry.
-        /// </exception>
-        /// <exception cref="RepositoryCorruptedException">
-        ///     Throws when the plugin registry is in an invalid state.
-        /// </exception>
-        /// <exception cref="OperationCanceledException">
-        ///     Throws when the operation was canceled.
-        /// </exception>
-        Task CleanupObsoletePluginsAsync(CancellationToken cancellationToken);
 
         /// <summary>
         ///    Raised when an error occurs interacting with a paticular <see cref="PluginSource"/>.
