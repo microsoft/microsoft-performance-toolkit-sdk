7786f93f53dfeccf2309b050297b3e56a8b61d08
Jayson Maxson <jmaxson@ntdev.microsoft.com>
2023-04-05 12:35:31 -0700

    One of the fake test plugin modules is netstandard for additional coverage.

--- src/TestPluginAssembly1/FakePlugin1.cs
+++ src/TestPluginAssembly1/FakePlugin1.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+using System.Collections.Generic;
+using System.IO;
 using Microsoft.Performance.SDK.Processing;
 
 namespace TestPluginAssembly1
