570af71d33adc8369f20a6cd08b1d0e755bb92f7
Helen Zhang <helzhang@microsoft.com>
2023-04-07 11:23:57 -0700

    PR comment

--- src/PluginsSystem/Microsoft.Performance.Toolkit.Plugins.Runtime/Installation/Checksum/SHA256DirectoryChecksumCalculator.cs
+++ src/PluginsSystem/Microsoft.Performance.Toolkit.Plugins.Runtime/Installation/Checksum/SHA256DirectoryChecksumCalculator.cs
@@ -1,8 +1,12 @@
 ﻿// Copyright (c) Microsoft Corporation.
 // Licensed under the MIT License.
 
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
 using System.Threading.Tasks;
-using Microsoft.Performance.Toolkit.Plugins.Core;
+using Microsoft.Performance.SDK;
 
 namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Installation
 {
@@ -12,10 +16,39 @@ namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Installation
     public sealed class SHA256DirectoryChecksumCalculator
         : IDirectoryChecksumCalculator
     {
-        /// <inheritdoc />
+        /// <summary>
+        ///     Calculates the hash of all files under the given directory including the file paths.
+        /// </summary>
+        /// <param name="directory">
+        ///     Path to the directory.
+        /// </param>
+        /// <returns>
+        ///     The combined SHA256 hash of all files including the file paths.
+        /// </returns>
         public async Task<string> GetDirectoryChecksumAsync(string directory)
         {
-            return await HashUtils.GetDirectoryHashAsync(directory);
+            Guard.NotNull(directory, nameof(directory));
+            Guard.IsTrue(Directory.Exists(directory), $"Directory {directory} doesn't exist.");
+
+            using (var sha256 = SHA256.Create())
+            using (var cs = new CryptoStream(Stream.Null, sha256, CryptoStreamMode.Write))
+            {
+                foreach (string filePath in Directory.EnumerateFiles(directory))
+                {
+                    // Hash file paths
+                    byte[] pathBytes = Encoding.UTF8.GetBytes(filePath);
+                    cs.Write(pathBytes, 0, pathBytes.Length);
+
+                    // Hash file content
+                    using (FileStream stream = File.OpenRead(filePath))
+                    {
+                        await stream.CopyToAsync(cs);
+                    }
+                }
+
+                cs.FlushFinalBlock();
+                return BitConverter.ToString(sha256.Hash).Replace("-", "").ToLower();
+            }
         }
     }
 }
