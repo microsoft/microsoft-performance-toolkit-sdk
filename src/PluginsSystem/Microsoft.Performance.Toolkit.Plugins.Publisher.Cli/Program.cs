72997d7e43b29bc407af18c42522ff3e0cae67ea
Helen Zhang <helzhang@microsoft.com>
2023-04-17 18:41:05 -0700

    Add CLI

---
+++ src/PluginsSystem/Microsoft.Performance.Toolkit.Plugins.Publisher.Cli/Program.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+namespace Microsoft.Performance.Toolkit.Plugins.Publisher.Cli
+{
+    public sealed class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandDispatcher.Default.Main(args);
+        }
+    }
+}
