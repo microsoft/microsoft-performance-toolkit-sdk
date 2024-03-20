c226f65484ef07128e4694ba7cda3eed9cb12e19
Luke Bordonaro <lukebo@microsoft.com>
2024-03-20 08:44:53 -0700

    Prevent installing plugin over previous installation folder

---
+++ src/PluginsSystem/Microsoft.Performance.Toolkit.Plugins.Runtime/Validation/PreviousInstallationFolderValidator.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+using System;
+using System.IO;
+using Microsoft.Performance.SDK;
+using Microsoft.Performance.SDK.Processing;
+using Microsoft.Performance.SDK.Runtime;
+using Microsoft.Performance.Toolkit.Plugins.Core.Metadata;
+using Microsoft.Performance.Toolkit.Plugins.Runtime.Installation;
+using NuGet.Versioning;
+
+namespace Microsoft.Performance.Toolkit.Plugins.Runtime.Validation
+{
+    /// <summary>
+    ///     Validates a plugin's <see cref="PluginMetadata.SdkVersion"/> is compatible with
+    ///     the currently running's SDK version.
+    /// </summary>
+    public class PreviousInstallationFolderValidator
+        : IPluginValidator
+    {
+        private readonly ILogger logger;
+        private readonly IPluginsStorageDirectory pluginsStorageDirectory;
+
+        public PreviousInstallationFolderValidator(
+            ILogger logger,
+            IPluginsStorageDirectory pluginsStorageDirectory)
+        {
+            this.logger = logger;
+            this.pluginsStorageDirectory = pluginsStorageDirectory;
+        }
+
+        public ErrorInfo[] GetValidationErrors(PluginMetadata pluginMetadata)
+        {
+            var dir = this.pluginsStorageDirectory.GetRootDirectory(pluginMetadata.Identity);
+
+            if (Directory.Exists(dir))
+            {
+                string errorMessage =
+                    $"The plugin {pluginMetadata.Identity} has already been previously installed to {dir} and has not been removed. Please remove the folder and try again.";
+
+                this.logger.Error(errorMessage);
+
+                return new []{ new ErrorInfo(ErrorCodes.PLUGINS_VALIDATION_PreviousInstallationFolderExists, errorMessage) };
+            }
+
+            return Array.Empty<ErrorInfo>();
+        }
+    }
+}
