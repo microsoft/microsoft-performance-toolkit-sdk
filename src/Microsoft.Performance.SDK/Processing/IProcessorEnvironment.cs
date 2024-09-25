5795663ae1c4b1071e1d270b923548addab512cc
Jayson Maxson <jmaxson@microsoft.com>
2024-09-25 15:41:43 -0700

    Move IProcessorEnvironmentV2 into separate file.

--- src/Microsoft.Performance.SDK/Processing/IProcessorEnvironment.cs
+++ src/Microsoft.Performance.SDK/Processing/IProcessorEnvironment.cs
@@ -44,21 +44,4 @@ namespace Microsoft.Performance.SDK.Processing
         /// </exception>
         IDynamicTableBuilder RequestDynamicTableBuilder(TableDescriptor descriptor);
     }
-
-    /// <summary>
-    ///     Presents the services offered by the application for
-    ///     a given session. You use this interface to interact with
-    ///     the caller on a session by session basis.
-    /// </summary>
-    public interface IProcessorEnvironmentV2
-        : IProcessorEnvironment
-    {
-        /// <summary>
-        ///     Provides the interface to be used to notify that data in
-        ///     a table has changed.
-        ///     <para/>
-        ///     This may be <c>null</c>.
-        /// </summary>
-        ITableDataSynchronizationV2 TableDataSynchronizer { get; }
-    }
 }
