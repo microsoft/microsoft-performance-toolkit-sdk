16f6629c2bdf5c223c33d5469dfe00a982448656
Luke Bordonaro <lukebo@microsoft.com>
2024-11-14 09:55:22 -0800

    Update gitignore for Jetbrains IDEs

---
+++ src/Microsoft.Performance.SDK/Processing/ColumnBuilding/ColumnVariantProperties.cs
@@ -0,0 +1,17 @@
+namespace Microsoft.Performance.SDK.Processing.ColumnBuilding;
+
+/// <summary>
+///     Describes the metadata properties of a data column represented by a column variant.
+/// </summary>
+public record ColumnVariantProperties
+{
+    /// <summary>
+    ///     Gets or initializes the human-readable name of the column variant.
+    /// </summary>
+    public string Name { get; init; }
+
+    /// <summary>
+    ///     Gets or initializes an optional human-readable description of the column variant.
+    /// </summary>
+    public string Description { get; init; }
+}
