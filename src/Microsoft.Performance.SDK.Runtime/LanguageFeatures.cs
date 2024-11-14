63fe248c7fddd7b63e246996dcf3692e0c076b2f
Luke Bordonaro <lukebo@microsoft.com>
2024-11-14 10:52:06 -0800

    Fix build

--- src/Microsoft.Performance.SDK.Runtime/LanguageFeatures.cs
+++ src/Microsoft.Performance.SDK.Runtime/LanguageFeatures.cs
@@ -10,5 +10,13 @@ namespace System.Runtime.CompilerServices
 
     public class RequiredMemberAttribute : Attribute { }
 
-    public class CompilerFeatureRequiredAttribute : Attribute { }
+    public class CompilerFeatureRequiredAttribute : Attribute
+    {
+        public CompilerFeatureRequiredAttribute(string name) { }
+    }
+}
+
+namespace System.Diagnostics.CodeAnalysis
+{
+    public class SetsRequiredMembersAttribute : Attribute {}
 }
