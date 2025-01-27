b3d7c29c10531bbe9ded91fb3e83499da2b05ee9
Luke Bordonaro <lukebo@microsoft.com>
2025-01-27 15:51:23 -0800

    Docstrings

--- src/Microsoft.Performance.SDK.Runtime/Options/ProcessingSourcePluginOptionsProvider.cs
+++ src/Microsoft.Performance.SDK.Runtime/Options/ProcessingSourcePluginOptionsProvider.cs
@@ -12,11 +12,17 @@ namespace Microsoft.Performance.SDK.Runtime.Options;
 ///     provided option is a clone of the original. This is to ensure that the original option instances returned
 ///     by the <see cref="IProcessingSource"/> instances are not modified or used by the implementing plugin.
 /// </summary>
-public sealed class ProcessingSourcePluginOptionsProvider
+internal sealed class ProcessingSourcePluginOptionsProvider
     : PluginOptionsRegistry.Provider
 {
     private readonly IReadOnlyCollection<IProcessingSource> processingSources;
 
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="ProcessingSourcePluginOptionsProvider"/> class.
+    /// </summary>
+    /// <param name="processingSources">
+    ///     The <see cref="IProcessingSource"/> instances from which to provide options.
+    /// </param>
     public ProcessingSourcePluginOptionsProvider(IReadOnlyCollection<IProcessingSource> processingSources)
     {
         this.processingSources = processingSources;
