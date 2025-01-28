99c8bf7597a59b014d43a9c1f6ae934cb12a7564
Luke Bordonaro <lukebo@microsoft.com>
2025-01-28 09:11:13 -0800

    Docstrings

--- src/Microsoft.Performance.Toolkit.Engine/Engine.cs
+++ src/Microsoft.Performance.Toolkit.Engine/Engine.cs
@@ -780,8 +780,8 @@ namespace Microsoft.Performance.Toolkit.Engine
                     ? createInfo.ApplicationName
                     : string.Empty;
 
-                // TODO (options): we also need to support options from plugins without processing sources
-                // TODO (options): support saving these options
+                // TODO (#404): we also need to support options from plugins without processing sources
+                // TODO (#404): support saving these options
                 var pluginOptionsSystem = PluginOptionsSystem.CreateUnsaved();
                 pluginOptionsSystem.RegisterOptionsFrom(instance.ProcessingSourceReferences.Select(psr => psr.Instance));
 
